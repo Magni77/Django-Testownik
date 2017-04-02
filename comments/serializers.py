f241453fd6ec46c32d68f27f00c76b93a3d255f8
Magni77 <michalmagni@gmail.com>
2017-04-02 20:42:31 +0200

    User serialzier

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.fields import ReadOnlyField
+
 from .models import CommentModel
 
 
 class CommentDetailSerializer(serializers.ModelSerializer):
+    user = ReadOnlyField(source='user.username')
+
     class Meta:
         model = CommentModel
         fields = [
@@ -15,6 +19,8 @@ class CommentDetailSerializer(serializers.ModelSerializer):
 
 
 class CommentCreateSerializer(serializers.ModelSerializer):
+    user = ReadOnlyField(source='user.username')
+
     class Meta:
         model = CommentModel
         fields = [
