a25237a4ef13d8a73dcadaff29970320802682b1
Magni77 <michalmagni@gmail.com>
2017-04-09 16:35:52 +0200

    Private tests views

--- testownikPWR/settings.py
+++ testownikPWR/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     # 3rd party's apps
     'corsheaders',
     'rest_framework',
+    'storages',
 
     # My apps
     'accounts',
@@ -161,19 +162,52 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
+#
+#
+# STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+#
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#
+# #STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
+#
+# MEDIA_URL = "/media/"
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+#AWS_S3_SECURE_URLS = False       # use http instead of https
+
+AWS_ACCESS_KEY_ID = "AKIAJCRAGA5ZCS2OL42Q"
+AWS_SECRET_ACCESS_KEY = "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"
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+AWS_FILE_EXPIRE = 200
+AWS_PRELOAD_METADATA = True
+AWS_QUERYSTRING_AUTH = True
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'testownikPWR.utils.MediaS3BotoStorage'
+STATICFILES_STORAGE = 'testownikPWR.utils.StaticS3BotoStorage'
+AWS_STORAGE_BUCKET_NAME = 'bucket-tesownik-ireland'
+S3DIRECT_REGION = 'eu-west-1'
+AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = MEDIA_URL
+STATIC_URL = S3_URL + 'static/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
-#STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
+import datetime
 
-MEDIA_URL = "/media/"
+two_months = datetime.timedelta(days=61)
+date_two_months_later = datetime.date.today() + two_months
+expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
+AWS_HEADERS = {
+    'Expires': expires,
+    'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+
+}
